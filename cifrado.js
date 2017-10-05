a2140b202dd7cbff6c4f5def64797a4036efe6eb
gabycasti <castillg@gmail.com>
2017-10-04 22:16:32 -0300

    Cambios Actualizados

--- cifrado.js
+++ cifrado.js
@@ -1,15 +1,15 @@
-//function principal () {
+function principal () {
 	do{
 
 		var resp = prompt("Indique el numero de lo que desea hacer: 1)Encriptar - 2)Desencriptar");
 
-		if(resp != ""){
+		if(resp != ""){ // Respuesta debe ser distinto a vacio
 			if (resp == "1"){
 				var palabra = prompt("ingrese la palabra que desea  Encriptar");
-				cipher(palabra);
+				cipher(palabra); // llamo la función Encriptar
 			} else if (resp == "2") {
 				var palabra = prompt("ingrese la palabra que desea  Desencriptar");
-				decipher(palabra);
+				decipher(palabra); // llamo a la función Desencriptar
 			} else {
 				alert("Ingrese una opción válida");
 			}
@@ -27,9 +27,9 @@ function cipher(palabra){
     palabra = palabra.toUpperCase();
     
 	for (i=0; i<palabra.length;i++){
-		cod = palabra.charCodeAt(i);
-		cod = ((cod-65+33)%26)+65;
-		result+= String.fromCharCode(cod); 
+		cod = palabra.charCodeAt(i); // Luego de recorrer la palabra le aplico la función charCodeAt la misma pide un indice por eso se le coloca i
+		cod = ((cod-65+33)%26)+65; // aplico la formula para obtener el código ascii
+		result+=String.fromCharCode(cod); 
 	}
 
 	alert(result);
@@ -40,14 +40,19 @@ function decipher(palabra){
 	var result = " ";
 	var cod = 0;
 
-	palabra = palabra.toUpperCase();
-    
-	for (i=0; i<palabra.length;i++){
-		cod = palabra.charCodeAt(i);
-		cod = (((65-cod)+33)%26)+65;
-		result+= String.fromCharCode(cod); 
+	palabra = palabra.toUpperCase(); // convierto la palabra en mayuscula
+
+   
+	for (i=0; i<palabra.length;i++){ 
+		cod = palabra.charCodeAt(i); //método devuelve un número indicando el valor Unicode del carácter en el índice proporcionado.
+		cod = ((cod-65-7+26)%26)+65;
+		result+= String.fromCharCode(cod); // me devuelve una cadena creada mediante el uso de una secuencia de valores Unicode especificada.
+                                           // Sintaxis
+
 	}
 
 	palabra = palabra.toLowerCase();
 	alert(result);
+ }
 }
+principal();
